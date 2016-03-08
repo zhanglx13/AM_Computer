c608bb16339644642fa1ca8819a1ded9f97ec71b
Lixun Zhang <zhanglx@utexas.edu>
2016-03-08 15:48:03 -0600

    Add imageTest.cu file

---
+++ imageTest/imageTest.cu
@@ -0,0 +1,18 @@
+/**
+ * Test of an image processing
+ *
+ * This sample 
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+#include <cuda_runtime.h>
+#include <helper_cuda.h> /* for function checkCudaErrors() */
+
+__global__ void 
+imageTest()
+{
+
+}
+
