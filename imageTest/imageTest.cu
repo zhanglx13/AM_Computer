5ccc74bfe8b592a3015f427a3d9aaf05aae50287
Lixun <zhanglx@utexas.edu>
2016-03-14 15:50:11 -0500

    Finish the experiment of image processing on GPU and CPU
    The pixel size can be 1 byte, 2 bytes, 4 bytes, and 8 bytes.
    Add a script to run the experiment and save the result in text files.

--- imageTest/imageTest.cu
+++ imageTest/imageTest.cu
@@ -17,32 +17,39 @@
 #include <cuda.h>
 #include <helper_functions.h> /* for StopWatchInterface*/
 
-// define, experiment
-#define MEMORY_ITERATIONS   10
-#define START_SIZE          ( 1 << 10 )     //1 KB
-#define END_SIZE_NVS        ( 1 << 29 )     //512 MB
-#define END_SIZE_TITAN      ( 1 << 32 )     //4 GB
+#include <math.h>   /* for log2 function */
 
+// define, experiment
+#define KERNEL_ITERATIONS   100
+#define START_SIZE          ( 1 << 10 )     //1 KB
+#define END_SIZE_NVS        ( 1 << 28 )     //256 MB    (1/4 of the device memory)
+#define END_SIZE_TITAN      ( 1 << 31 )     //2 GB      (1/3 of the device memory)
+
+#define BLOCK_SIZE          ( 1 << 7 )      //128 threads per block. This should be tested and tuned
+#define GRID_X              ( 1 << 11 )     //2048
+#define GRID_LIMIT          ( 1 << 16 - 1 ) //65535 for nvs and titan
+
+// define, pixel operation
+#define PIXEL_OP(TYPE, idata, odata)    odata = (unsigned TYPE)(idata + 1);  
 // define, kernel functions
+/* 
+ * We first try to launch 1D block and 1D grid.
+ * If the gridSize is bigger than the limit (65535), try to launch 2D grid
+ */
 #define KERNEL_DEF(SIZE, TYPE)                                                                                  \
     __global__ void imageTest_##SIZE(unsigned TYPE *d_idata, unsigned TYPE *d_odata, unsigned int nr_pixels)    \
     {                                                                                                           \
-        unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;                                               \
-        if (tid < nr_pixels)                                                                                    \
-            d_odata[tid] = (unsigned TYPE)(d_idata[tid] + 1);                                                   \
+        unsigned int i = gridDim.x * blockIdx.y + blockIdx.x * blockDim.x + threadIdx.x;                        \
+        if (i < nr_pixels)                                                                                      \
+            d_odata[i] = (unsigned TYPE)(d_idata[i] + 1);                                                       \
     }                                                                                                           \
 
-#define KERNEL_CALL(SIZE)                                                   \
-    imageTest_##SIZE<<<gridSize, blockSize>>>(d_idata, d_odata, nr_pixels); \
+// define kernel functions for different pixel sizes
+KERNEL_DEF(1, char)
+KERNEL_DEF(2, short)
+KERNEL_DEF(4, int)
+KERNEL_DEF(8, long long)
 
-#define ALLOCATE_AND_INIT(TYPE)                                                     \
-    unsigned TYPE *h_idata, *d_idata, *d_odata;                                     \
-    h_idata = (unsigned TYPT)malloc(memSize);                                       \
-    checkCudaErrors(cudaMalloc((void**)&d_idata, memSize));                         \
-    checkCudaErrors(cudaMalloc((void**)&d_idata, memSize));                         \
-    for (unsigned int i = 0 ; i < nr_pixels ; i ++)                                 \
-        h_idata[i] = (unsigned TYPE)(i);                                            \
-    checkCudaErrors(cudaMemcpy(d_idata, h_idata, memSize, cudaMemcpyHostToDevice))  \
 
 // enums
 enum {  BYTE = 1,   // 8-bit pixels, unsigned char
@@ -51,21 +58,103 @@ enum {  BYTE = 1,   // 8-bit pixels, unsigned char
         BYTE8 = 8   // 64-bit pixels, unsigned long long
 };
 
+const char *sType[] = 
+{
+    "char",
+    "short",
+    "int",
+    "long long",
+    NULL
+};
+
 ////////////////////////////////////////////////////////////////////////////////////////
 // Function declarations, forward
 ////////////////////////////////////////////////////////////////////////////////////////
-float imageProcessingGPU(unsigned int memSize, unsigned int pixelSize); 
-float imageProcessingCPU(unsigned int memSize, unsigned int pixelSize); 
+void printResults(double *GPUTime, double *CPUTime, unsigned int count);
+// Declaration of imageProcessingGPU functions
+/* cudaEventSynchronize(stop) should be called after recording stop
+ * because kernel launching is asynchronous.
+ */
+#define IMAGEPROCESSINGGPU(TYPE, SIZE)                                          \
+    float imageProcessingGPU_##SIZE(unsigned int memSize)                       \
+    {                                                                           \
+        float elapsedTimeInMs = 0.0f;                                           \
+        cudaEvent_t start, stop;                                                \
+        checkCudaErrors(cudaEventCreate(&start));                               \
+        checkCudaErrors(cudaEventCreate(&stop));                                \
+                                                                                \
+        unsigned int nr_pixels = memSize / SIZE;                                \
+        dim3 grid(1,1,1);                                                       \
+        dim3 block(1,1,1);                                                      \
+        block.x = (nr_pixels >= BLOCK_SIZE)? BLOCK_SIZE : nr_pixels;            \
+        unsigned int nr_blocks = (nr_pixels + block.x - 1) / block.x;           \
+        grid.x = (nr_blocks <= GRID_X)? nr_blocks : GRID_X;                     \
+        grid.y = (nr_blocks + grid.x - 1) / grid.x;                             \
+        /*printf("blocksize = %d\tgridx = %d\tgrid.y = %d\n", block.x, grid.x, grid.y);            */ \
+                                                                                \
+        unsigned TYPE *h_idata, *d_idata, *d_odata;                             \
+        h_idata = (unsigned TYPE *)malloc(memSize);                             \
+        checkCudaErrors(cudaMalloc((void**)&d_idata, memSize));                 \
+        checkCudaErrors(cudaMalloc((void**)&d_odata, memSize));                 \
+        for (unsigned int i = 0 ; i < nr_pixels ; i ++)                         \
+            h_idata[i] = (unsigned TYPE)(i);                                    \
+        checkCudaErrors(cudaMemcpy(d_idata, h_idata, memSize,                   \
+                                   cudaMemcpyHostToDevice));                    \
+                                                                                \
+        checkCudaErrors(cudaEventRecord(start, 0));                             \
+        imageTest_##SIZE<<<grid, block>>>(d_idata, d_odata, nr_pixels);         \
+        checkCudaErrors(cudaEventRecord(stop, 0));                              \
+        cudaEventSynchronize(stop);                                             \
+        checkCudaErrors(cudaEventElapsedTime(&elapsedTimeInMs, start, stop));   \
+                                                                                \
+        checkCudaErrors(cudaEventDestroy(start));                               \
+        checkCudaErrors(cudaEventDestroy(stop));                                \
+        free(h_idata);                                                          \
+        checkCudaErrors(cudaFree(d_idata));                                     \
+        checkCudaErrors(cudaFree(d_odata));                                     \
+        return elapsedTimeInMs;                                                 \
+}                                                                               \
+
+IMAGEPROCESSINGGPU(char, 1)
+IMAGEPROCESSINGGPU(short, 2)
+IMAGEPROCESSINGGPU(int, 4)
+IMAGEPROCESSINGGPU(long long, 8)
+
+// Declaration of imageProcessingCPU functions
+#define IMAGEPROCESSINGCPU(TYPE, SIZE)                                          \
+    float imageProcessingCPU_##SIZE(unsigned int memSize)                       \
+    {                                                                           \
+        StopWatchInterface *timer = NULL;                                       \
+        float elapsedTimeInMs = 0.0f;                                           \
+        sdkCreateTimer(&timer);                                                 \
+                                                                                \
+        unsigned int nr_pixels = memSize / SIZE;                                \
+        unsigned TYPE *h_idata, *h_odata;                                       \
+        h_idata = (unsigned TYPE *)malloc(memSize);                             \
+        h_odata = (unsigned TYPE *)malloc(memSize);                             \
+                                                                                \
+        for (unsigned int i = 0 ; i < nr_pixels ; i ++)                         \
+            h_idata[i] = (unsigned TYPE)(i);                                    \
+                                                                                \
+        sdkStartTimer(&timer);                                                  \
+        for (unsigned int i = 0 ; i < nr_pixels ; i ++)                         \
+            PIXEL_OP(TYPE, h_idata[i], h_odata[i])                              \
+        sdkStopTimer(&timer);                                                   \
+        elapsedTimeInMs = sdkGetTimerValue(&timer);                             \
+                                                                                \
+        free(h_idata);                                                          \
+        free(h_odata);                                                          \
+        sdkDeleteTimer(&timer);                                                 \
+        return elapsedTimeInMs;                                                 \
+    }                                                                           \
+
+IMAGEPROCESSINGCPU(char, 1)
+IMAGEPROCESSINGCPU(short, 2)
+IMAGEPROCESSINGCPU(int, 4)
+IMAGEPROCESSINGCPU(long long, 8)
 
 
-// define kernel functions for different pixel sizes
-KERNEL_DEF(BYTE, char)
-KERNEL_DEF(BYTE2, short)
-KERNEL_DEF(BYTE4, int)
-KERNEL_DEF(BYTE8, long long)
-
-
-int main(int argc, char **argv)
+int main(const int argc, const char **argv)
 {
     /* check byte size assumptions 
      * unsigned char = 1 byte
@@ -73,19 +162,19 @@ int main(int argc, char **argv)
      * unsigned int = 4 bytes
      * unsigned long long = 8 bytes
      */
-    if (sizeof(unsigned char) != 1){
+    if (sizeof(unsigned char) != BYTE){
         printf("unsigned char size != 1\n");
         exit(EXIT_FAILURE);
     }
-    if (sizeof(unsigned short) != 2){
+    if (sizeof(unsigned short) != BYTE2){
         printf("unsigned short size != 2\n");
         exit(EXIT_FAILURE);
     }
-    if (sizeof(unsigned int) != 4){
+    if (sizeof(unsigned int) != BYTE4){
         printf("unsigned int size != 4\n");
         exit(EXIT_FAILURE);
     }
-    if (sizeof(unsigned long long) != 8){
+    if (sizeof(unsigned long long) != BYTE8){
         printf("unsigned long long size != 8\n");
         exit(EXIT_FAILURE);
     }
@@ -96,56 +185,67 @@ int main(int argc, char **argv)
 #else
     int end = END_SIZE_TITAN;
 #endif
+    
+    unsigned int pixelSize = 0;
+    if (checkCmdLineFlag(argc, argv, "char"))
+        pixelSize = 1;
+    if (checkCmdLineFlag(argc, argv, "short"))
+        pixelSize = 2;
+    if (checkCmdLineFlag(argc, argv, "int"))
+        pixelSize = 4;
+    if (checkCmdLineFlag(argc, argv, "long"))
+        pixelSize = 8;
+    // number of experiments
+    unsigned int count = (unsigned int)(log2((float)end) - log2((float)start)) + 1;
+    double *GPUTime = (double*)malloc(sizeof(double)*count);
+    double *CPUTime = (double*)malloc(sizeof(double)*count);
+    unsigned int i = 0;
+    for (unsigned int memSize = start ; memSize <= end ; memSize <<= 1){
+        switch(pixelSize){
+            case 1:
+                GPUTime[i] = imageProcessingGPU_1(memSize);
+                CPUTime[i] = imageProcessingCPU_1(memSize);
+                break;
+            case 2:
+                GPUTime[i] = imageProcessingGPU_2(memSize);
+                CPUTime[i] = imageProcessingCPU_2(memSize);
+                break;
+            case 4:
+                GPUTime[i] = imageProcessingGPU_4(memSize);
+                CPUTime[i] = imageProcessingCPU_4(memSize);
+                break;
+            case 8:
+                GPUTime[i] = imageProcessingGPU_8(memSize);
+                CPUTime[i] = imageProcessingCPU_8(memSize);
+                break;
+        }
+        i ++;
+    }
 
-    printf("%f\n", imageProcessingGPU(128, 4));
+    printResults(GPUTime, CPUTime, count);
+
+    // clean up memory
+    free(GPUTime);
+    free(CPUTime);
 
     exit(EXIT_SUCCESS);
 }    
 
-    
-
-float imageProcessingGPU(unsigned int memSize, unsigned int pixelSize)
+///////////////////////////////////////////////////////////////////////////
+//print results in a database format
+///////////////////////////////////////////////////////////////////////////
+void printResults(double *GPUTime, double *CPUTime, unsigned int count)
 {
-    StopWatchInterface *timer = NULL;
-    float elapsedTimeInMs = 0.0f;
-    
-    unsigned int nr_pixels = memSize / pixelSize;
-    // allocate host and device memory according to the pixel size
-    switch(pixelSize){
-        case 1:
-            ALLOCATE_AND_INIT(char)
-            break;
-        case 2:
-            ALLOCATE_AND_INIT(short)
-            break;
-        case 4:
-            ALLOCATE_AND_INIT(int)
-            break;
-        case 8:
-            ALLOCATE_AND_INIT(long long)
-            break;
-    }
+    printf("GPUTime:");
+    for (unsigned int i = 0 ; i < count ; i ++)
+        printf("   %.5f", GPUTime[i]);
+    printf("\n");
 
-    dim3 gridSize(1,1,1);
-    dim3 blockSize(32,1,1);
-    KERNEL_CALL(BYTE4)
-
-
-    return 0.0f;
+    printf("CPUTime:");
+    for (unsigned int i = 0 ; i < count ; i ++)
+        printf("   %.5f", CPUTime[i]);
+    printf("\n");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
